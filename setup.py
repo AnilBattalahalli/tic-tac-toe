64d2719d8c3b652b217ec1ca9d463958cdea7745
Anil Battalahalli <anil.battalahalli@gmail.com>
2018-06-30 11:10:40 +0530

    Built python3 Wheels

---
+++ setup.py
@@ -0,0 +1,19 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="g-tictactoe",
+    version="0.0.1",
+    author="Anil Battalahalli",
+    author_email="anil.battalahalli@gmail.com",
+    description="A Generic NxN TicTacToe Library",
+    url="https://github.com/AnilBattalahalli/tic-tac-toe/",
+    packages=setuptools.find_packages(),
+    classifiers=(
+        "Programming Language :: Python :: 3",
+        "Operating System :: OS Independent",
+    ),
+)
+
