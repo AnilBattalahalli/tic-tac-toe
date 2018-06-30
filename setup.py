c78c19fb36d0d1e47b5fc164bc282b13a52d490e
Anil Battalahalli <anil.battalahalli@gmail.com>
2018-06-30 11:57:33 +0530

    Fixed Package Names

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="g_tictactoe",
-    version="1.0.1",
+    version="1.0.2",
     author="Anil Battalahalli",
     author_email="anil.battalahalli@gmail.com",
     description="A Generic NxN TicTacToe Library",
